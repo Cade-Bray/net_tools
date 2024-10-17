3d3f8cf9f4c27faa356d7558b8d5f60b4f1780b8
Cade Bray <bray.cade@gmail.com>
2024-10-17 13:40:16 -0600

    Added a dope CLI ascii art

--- src/main/java/org/bray/Main.java
+++ src/main/java/org/bray/Main.java
@@ -13,8 +13,20 @@ public class Main {
      */
     private static final Logger logger = Logger.getLogger(Main.class.getName());
 
+    public static void welcome() {
+        System.out.println("___________________________________________________");
+        System.out.println("    _   ______________ __________  ____  __   _____");
+        System.out.println("   / | / / ____/_  __//_  __/ __ \\/ __ \\/ /  / ___/");
+        System.out.println("  /  |/ / __/   / /    / / / / / / / / / /   \\__ \\");
+        System.out.println(" / /|  / /___  / /    / / / /_/ / /_/ / /______/ /");
+        System.out.println("/_/ |_/_____/ /_/____/_/  \\____/\\____/_____/____/");
+        System.out.println("               /_____/                             ");
+        System.out.println("_______________________________v.0.1.0-alpha_______");
+    }
+
     public static void main(String[] args) {
         // Setting logging level to warning is recommended.
         logger.setLevel(Level.WARNING);
+        welcome();
         }
     }
